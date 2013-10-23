f78805e83acd06cdbcb1950efe2899e860b42f33
Alan Meadows <alan@nephoscale.com>
2013-10-23 13:27:03 -0700

    Correct quotes in wp-config.php

--- wordpress_single_stack.cs
+++ wordpress_single_stack.cs
@@ -144,21 +144,21 @@ rm /tmp/setup.mysql
 
 cat <<\EOF>/var/www/wordpress/wp-config.php
 <?php
-define('DB_NAME',       {{ wordpress_db_name }});
-define('DB_USER',       {{ wordpress_db_username }});
-define('DB_PASSWORD',   {{ wordpress_db_password }});
+define('DB_NAME',       '{{ wordpress_db_name }}');
+define('DB_USER',       '{{ wordpress_db_username }}');
+define('DB_PASSWORD',   '{{ wordpress_db_password }}');
 define('DB_HOST',       'localhost');
 define('DB_CHARSET',    'utf8');
 define('DB_COLLATE',    '');
 
-define('AUTH_KEY',         {{ wordpress_auth_key }});
-define('SECURE_AUTH_KEY',  {{ wordpress_secure_auth_key }});
-define('LOGGED_IN_KEY',    {{ wordpress_logged_in_key }});
-define('NONCE_KEY',        {{ wordpress_nonce_key }});
-define('AUTH_SALT',        {{ wordpress_auth_salt }});
-define('SECURE_AUTH_SALT', {{ wordpress_secure_auth_salt }});
-define('LOGGED_IN_SALT',   {{ wordpress_logged_in_salt }});
-define('NONCE_SALT',       {{ wordpress_nonce_salt }});
+define('AUTH_KEY',         '{{ wordpress_auth_key }}');
+define('SECURE_AUTH_KEY',  '{{ wordpress_secure_auth_key }}');
+define('LOGGED_IN_KEY',    '{{ wordpress_logged_in_key }}');
+define('NONCE_KEY',        '{{ wordpress_nonce_key }}');
+define('AUTH_SALT',        '{{ wordpress_auth_salt }}');
+define('SECURE_AUTH_SALT', '{{ wordpress_secure_auth_salt }}');
+define('LOGGED_IN_SALT',   '{{ wordpress_logged_in_salt }}');
+define('NONCE_SALT',       '{{ wordpress_nonce_salt }}');
 
 $table_prefix  = 'wp_';
 define('WPLANG', '');
